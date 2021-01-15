262f691e6a3bb3deaa38a457dc4368caf8696b9a
Khumairi Zulhizmarch <khumairiari21@gmail.com>
2021-01-16 03:24:31 +0700

    Add project files.

---
+++ ServerChatApp_028_KhumairiZulhizmarch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerChatApp_028_KhumairiZulhizmarch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost hostObject = null;
+
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Serves On, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
